2036f186d8cf23d641be133dd7a47be716ee2fec
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2026-01-10 16:58:41 -0500

    Update moves to 1.0.6

--- Casks/moves.rb
+++ Casks/moves.rb
@@ -1,6 +1,6 @@
 cask "moves" do
-  version "1.0.4"
-  sha256 "ff6783a2a24fe75b129e79a90bddb0f785ec6bac0855be6d127681172f5924fd"
+  version "1.0.6"
+  sha256 "235a3d098a7b2383ab6f76e81c646d98fed9eb4b4c649ba1c4b20ad5ea8c5770"
 
   url "https://github.com/farzadmf/Moves/releases/download/v#{version}/Moves.zip"
   name "Moves"
