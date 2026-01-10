466eab92069017daa1a8e51da376c5ffabc7c776
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2026-01-10 15:34:21 -0500

    Add moves cask

---
+++ moves.rb
@@ -0,0 +1,10 @@
+cask "moves" do
+  version "1.0.2"
+  sha256 "ed5e5672349cf02f9ceb84c0a364168884470b4163e87b90120d5608da78d884"
+
+  url "https://github.com/farzadmf/Moves/releases/download/v#{version}/Moves.zip"
+  name "Moves"
+  homepage "https://github.com/farzadmf/Moves"
+
+  app "Moves.app"
+end
