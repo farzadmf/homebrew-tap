bf4b78652b4c26f0d156d8b2d298815e2ba5f3fb
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2026-01-23 01:05:32 -0500

    Update farzadmf-moves to 1.0.11

--- Casks/farzadmf-moves.rb
+++ Casks/farzadmf-moves.rb
@@ -1,6 +1,6 @@
 cask "farzadmf-moves" do
-  version "1.0.10"
-  sha256 "2c2bad19d95480ffa8324335f0eced5c32975a1f72a1ecb6d74f6514db35095b"
+  version "1.0.11"
+  sha256 "dfa46287c6f821abf6b8a0994953078a06ad2676fb02e0e55d430d04793d0db0"
 
   url "https://github.com/farzadmf/Moves/releases/download/v#{version}/Moves.zip"
   name "Moves"
